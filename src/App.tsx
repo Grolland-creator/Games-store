b2fa8d57ad05755f312fc33deeca9612ed6068d1
grolland <ilya.play111@gmail.com>
2023-07-17 15:14:52 +0300

    The first version of the finished site

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC, useEffect, useCallback } from 'react';
+import './App.scss';
+import Layout from "./components/Wrappers/Layout/Layout";
+import Navbar from './components/Navbar/Navbar';
+import AppRouter from './router/AppRouter';
+import Content from './components/Wrappers/Content/Content';
+import Footer from './components/Footer/Footer';
+import { useActions } from './hooks/redux';
+import UserService from './api/UserService';
+import HeaderMargin from './components/Navbar/HeaderMargin/HeaderMargin';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+   const { setUser, setIsAuth } = useActions();
+
+   const setUserFromdb = useCallback(async () => {
+      try {
+         const username = localStorage.getItem('username')
+         const response = await UserService.getUsers()
+         const User = response.data.find(user => (user.username === username));
+         if (User) {
+            setUser(User)
+            setIsAuth(true)
+         } else {
+            localStorage.removeItem('auth')
+            localStorage.removeItem('username')
+            setIsAuth(false)
+         }
+      } catch (e) {
+         localStorage.removeItem('auth')
+         localStorage.removeItem('username')
+         setIsAuth(false)
+      }
+   }, [setUser, setIsAuth])
+   
+   useEffect(() => {
+      if (localStorage.getItem('auth') && localStorage.getItem('username')) {
+         setUserFromdb()
+      }
+   }, [setUserFromdb])
+
+   return (
+      <Layout>
+         <HeaderMargin/>
+         <Navbar />
+         <Content>
+            <AppRouter />
+         </Content>
+         <Footer />
+      </Layout>
+   );
+};
 
 export default App;
