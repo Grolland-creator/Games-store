2112355d1cc7d85702696aadf9ff1efd128c6332
grolland <ilya.play111@gmail.com>
2023-07-27 02:32:28 +0300

    bug fix

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState, useCallback } from 'react';
+import { FC, useEffect, useState } from 'react';
 import GameService from '../../api/GameService';
 import { IGame } from '../../models/IGame';
 import { useFetching } from '../../hooks/useFetching';
